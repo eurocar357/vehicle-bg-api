8b0311aa72d15c38096b20272539886efe66c50a
Gabriel Rico de Matos <gabrielricodematos@MacBook-Air-de-Gabriel.local>
2025-08-07 17:08:00 -0300

    Fix: Corrected main.py by removing database imports

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import os
 import sys
-# DON'T CHANGE THIS !!!
+# DON\'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.photoroom import photoroom_bp
 
@@ -17,13 +16,6 @@ CORS(app)
 
 app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(photoroom_bp, url_prefix='/api/photoroom')
-
-# uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-with app.app_context():
-    db.create_all()
 
 @app.route('/', defaults={'path': ''}) 
 @app.route('/<path:path>')
